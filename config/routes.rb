7d028b9f3c86c30801d4cc5cb922d212c8b5918a
David Tu & Steven Leiva <pair+spacebear5000.stevenxl@devbootcamp.com>
2015-10-19 14:38:51 -0400

    Add an active collection route to the conversations resource (nested within topics resource)

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   root to: 'topics#index'
 
   resources :topics, only: [:index, :show] do
-    resources :conversations, only: [:show, :index]
+    resources :conversations, only: [:show, :index] do
+      collection do
+        get 'active'
+      end
+    end
   end
 end
