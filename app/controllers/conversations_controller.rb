56e6e0e982da0118c229b503692a7c20ad21870e
Peter Rosal & <pair+r05al.walsh9@devbootcamp.com>
2015-10-19 11:58:35 -0400

    Add recent action for conversations

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -7,4 +7,8 @@ class ConversationsController < ActionController::Base
     @messages = Conversation.find(params[:id]).messages.order(created_at: :desc)
   end
 
+  def recent
+    @recent_messages = Message.order(created_at: :desc).limit(25)
+  end
+
 end
