c5b64045a15c9be163d4bb0ea9092f14b75036a4
Peter Rosal & <pair+r05al.walsh9@devbootcamp.com>
2015-10-19 11:16:19 -0400

    Create topics controller

---
+++ app/controllers/topics_controller.rb
@@ -0,0 +1,12 @@
+class TopicsController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+
+
+
+  def index
+    @topics = Topic.all
+  end
+
+end
