ee90c55a05e3165cb23b8a3d18a41362ab7d534d
David Tu & Steven Leiva <pair+spacebear5000.stevenxl@devbootcamp.com>
2015-10-19 11:32:23 -0400

    Add the show route for the topics resource in the router.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'topics#index'
 
-  resources :topics, only: [:index] do
+  resources :topics, only: [:index, :show] do
     resources :conversations, only: [:show, :index]
   end
 end
