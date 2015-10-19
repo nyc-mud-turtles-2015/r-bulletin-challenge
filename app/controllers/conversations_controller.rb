a1bcf0fdb819f8288997550e6155331eb47b0827
Peter Rosal & <pair+r05al.walsh9@devbootcamp.com>
2015-10-19 12:32:08 -0400

    Inherit from ApplicationController

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,7 +1,4 @@
-class ConversationsController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+class ConversationsController < ApplicationController
 
   def show
     @messages = Conversation.find(params[:id]).messages.order(created_at: :desc)
