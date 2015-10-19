93ca2d09597f8d484494622aaf8bf58249157832
David Tisdall & Heather Lorenzo <pair+dtisdall.jeder28@devbootcamp.com>
2015-10-19 11:46:22 -0400

    Create show route in conversation_controller for show page

---
+++ app/controllers/conversations_controller.rb
@@ -0,0 +1,10 @@
+class ConversationsController < ApplicationController
+  def show
+    @conversation = find_convo
+  end
+
+  private
+  def find_convo
+    Conversation.find(params[:id])
+  end
+end
