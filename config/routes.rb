a342ec5f0cf1b222ba40199cf967170aa138f849
David Tu & Steven Leiva <pair+spacebear5000.stevenxl@devbootcamp.com>
2015-10-19 15:41:21 -0400

    add get and post route for creating new users

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,7 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :users, only: [:new, :create]
+  # resources :users, only: [:new, :create]
+  get '/register', to:'users#register'
+  post '/register', to:'users#signup'
 end
