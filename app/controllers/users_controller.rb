05f64d8fb1b2566fa5284c6dddda61c2e7134b26
Peter Rosal & <pair+r05al.walsh9@devbootcamp.com>
2015-10-19 12:31:39 -0400

    Add create action to users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,20 @@
-class UsersController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+class UsersController < ApplicationController
 
   def new
     @user = User.new
   end
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      redirect_to root_path
+    else
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:username, :email, :password)
+    end
 
 end
