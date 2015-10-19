9fe36e27eaeb112fd33810e442c66fc6de81bdb5
David Tu & Steven Leiva <pair+spacebear5000.stevenxl@devbootcamp.com>
2015-10-19 14:38:00 -0400

    Add an active action to the Conversations controller.

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -2,4 +2,8 @@ class ConversationsController < ApplicationController
   def show
     @messages = Conversation.find(params[:id]).messages.order(created_at: :asc)
   end
+
+  def active
+    @conversations = Topic.find(params[:topic_id]).conversations
+  end
 end
