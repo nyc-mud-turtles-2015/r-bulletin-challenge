67dc292a62270885c43a028313a6664ae73f686a
David Tu & Steven Leiva <pair+spacebear5000.stevenxl@devbootcamp.com>
2015-10-19 11:19:08 -0400

    Add conversations as a nested resource inside the topics resource.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root to: 'topics#index'
 
-  resources :topics, only: [:index]
+  resources :topics, only: [:index] do
+    resources :conversations, only: [:show, :index]
+  end
 end
