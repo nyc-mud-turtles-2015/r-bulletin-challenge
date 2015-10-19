edacdfc6e202bba008edfa48cb422dac71694693
Steven Leiva <leiva.steven@gmail.com>
2015-10-19 19:17:15 -0400

    Add logic to create a message associated with a specific user and conversation.
    Note that only a logged_in user can post, since a message requires a user_id to
    save.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,5 +4,19 @@ class MessagesController < ApplicationController
   end
 
   def create
+    conversation = Conversation.find_by(id: params[:conversation_id])
+    @message = conversation.messages.build(content: params[:message][:content])
+
+    @message.user_id = session[:user_id]
+
+    if @message.save
+      redirect_to conversation_path(conversation)
+    else
+      render :new
+    end
+  end
+
+  def message_params
+    params.require(:message).permit(:content)
   end
 end
