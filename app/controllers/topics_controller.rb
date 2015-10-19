be8ff5df4418d6e269e2811a12f83febd7c60dd4
David Tisdall & Heather Lorenzo <pair+dtisdall.jeder28@devbootcamp.com>
2015-10-19 11:45:55 -0400

    Create controller route for show page and arranging by conversation messages

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -2,4 +2,16 @@ class TopicsController < ApplicationController
   def index
     @topics = Topic.all
   end
+
+  def show
+    @topic = find_topic
+    @conversations = @topic.conversations.sort_by{|convo| convo.messages.last.created_at }.reverse
+  end
+
+  private
+
+  def find_topic
+    Topic.find(params[:id])
+  end
+
 end
