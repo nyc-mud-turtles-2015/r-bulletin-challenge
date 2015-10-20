2146753029a9faa04231e5567be95e2b412ffe61
Steven Leiva <leiva.steven@gmail.com>
2015-10-20 05:39:52 -0400

    Add a new and a create action the Conversations controller that allows a user to create a new conversation.

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -7,4 +7,27 @@ class ConversationsController < ApplicationController
   def active
     @conversations = Topic.find(params[:topic_id]).conversations
   end
+
+  def new
+    @selection_options = Topic.all.pluck(:name, :id)
+    @conversation = Conversation.new
+  end
+
+  def create
+    @conversation = Conversation.new(conversation_params)
+    @conversation.topic_id = params[:topic_id]
+    @conversation.user_id = session[:user_id]
+
+    if @conversation.save
+      redirect_to conversation_path(@conversation)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def conversation_params
+    params.require(:conversation).permit(:name)
+  end
 end
