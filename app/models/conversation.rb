d33c6c057d449dc758f7bb315e20ecd9c70aedb5
Peter Rosal <peter.rosal@gmail.com>
2015-10-19 20:49:05 -0400

    Add touch attr

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,19 +1,11 @@
 class Conversation < ActiveRecord::Base
-  belongs_to :topic
+  belongs_to :topic, touch: true
   belongs_to :author, class_name: 'User', foreign_key: :user_id
-  has_many :messages
+  has_many :messages, dependent: :destroy
 
   validates_presence_of :name
 
-  # gets conversations from latest n messages,
-  # want to get last n conversations from latest messages
-  # not sure how
   def self.recently_active(n)
-    Conversation
-    .includes(:messages)
-    .joins(:messages)
-    .order("messages.created_at DESC")
-    .group("conversations.id, messages.id")
-    .limit(n)
+    Conversation.order(updated_at: :desc).limit(n)
   end
 end
