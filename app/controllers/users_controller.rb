d0c7d5f39830878577cd30a4b5bf9a5999f4e8a4
Steven Leiva <leiva.steven@gmail.com>
2015-10-20 06:02:39 -0400

    Add a show action to the Users controller which will only show a user's own profile.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,17 @@ class UsersController < ApplicationController
     end
   end
 
+  def show
+    @user = User.find_by(id: params[:id])
+
+    if @user && @user.id == session[:user_id]
+      @conversations = Conversation.where("user_id = ?", @user)
+      render :show
+    else
+      redirect_to login_path
+    end
+  end
+
   private
 
   def user_params
