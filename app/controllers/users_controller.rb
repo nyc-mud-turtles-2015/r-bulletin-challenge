f59e30811220f314d7ad83a8b96600b3296fa99b
Jason & walsh9 <pair+agentwang007.walsh9@devbootcamp.com>
2015-10-21 17:38:18 -0400

    add paperclip for user avatars

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
 
   private
     def user_params
-      params.require(:user).permit(:username, :email, :password)
+      params.require(:user).permit(:username, :email, :password, :avatar)
     end
 
 end
