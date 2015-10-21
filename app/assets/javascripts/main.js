8cb730a5369a56af96facaa9e6f88661f8d808d0
Peter Rosal & Steven Leiva <pair+r05al.stevenxl@devbootcamp.com>
2015-10-21 15:23:54 -0400

    Finish Handlebars Spike.

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,20 +1,54 @@
 $(document).ready(function(){
-  $('form#new_message').on('submit', function(event){
-    event.preventDefault();
-    $form = $(event.target);
-    $.ajax({
-      url: $form.attr('action'),
-      method: $form.attr('method'),
-      dataType: 'HTML',
-      data: $form.serialize()
-    })
-    .then(function(response){
-      $('div#conversation').prepend(response);
-    })
-    .fail(function(jqXHR, textStatus, errorThrown){
-      var html = '<div id="flash"><span class="alert">' + jqXHR.responseText + '</span></div>';
-      $('#wrapper').prepend(html);
-    });
 
-  });
+  $('body').on("click", ".top-crimes-link", renderCrimes);
+  $('.top-crimes-list').on("click", ".crime", renderCriminals);
 });
+
+function renderCrimes(event) {
+  event.preventDefault();
+
+  var source = $('#crimes-template').html();
+  var template = Handlebars.compile(source);
+
+  // build ajax request
+  var jsonPrepend = "https://jsonp.afeld.me/?&url="
+  var url = jsonPrepend + event.target.href;
+  var dataType = 'json';
+
+  $.ajax({url: url, dataType: dataType})
+  .done(function(response) {
+    response.forEach(function(crime) {
+      var html = template(crime);
+
+      $('.top-crimes-list').append(html);
+    });
+  })
+  .fail(function(response) {
+    console.log("ajax failed", response);
+  });
+}
+
+function renderCriminals(event) {
+  event.preventDefault();
+
+  var source = $('#player-template').html();
+  var template = Handlebars.compile(source);
+  var crime = event.target.id;
+
+  // build ajax request
+  var jsonPrepend = "https://jsonp.afeld.me/?&url=";
+  var url = jsonPrepend + event.target.href;
+  var dataType = 'json';
+
+  $.ajax({url: url, dataType: dataType})
+  .done(function(response) {
+    response.forEach(function(criminal) {
+      var html = template(criminal);
+
+      $('.crime #' + crime).append(html);
+    });
+  })
+  .fail(function(response) {
+    console.log("ajax failed", response);
+  });
+}
